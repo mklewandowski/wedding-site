a226e7d7af85a8af914398e02772d641d9fcbbe2
mklewandowski <matt.kain.lewandowski@gmail.com>
2026-06-24 18:54:59 -0500

    feat(about): update photos and text

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid, Typography } from '@mui/material'
 
-const aboutPhotos = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg']
+const aboutPhotos = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg']
 
 export default function AboutPage() {
   return (
@@ -13,7 +13,9 @@ export default function AboutPage() {
         If you're invited to our wedding, then you already know us. 
         Bethany likes to make compost, hold her chickens, and sit in a cozy corner reading a book. 
         Matt likes to forage fruit, do handstands, and study videogame history.      
-        We met over 30 years ago on a cross country race course and after navigating the advenutres of a 2,200 mile long distance relationship, a pandemic, and being relatively tired middle aged adults, we made it to the finish line!
+        We met over 30 years ago at a Cross Country meet and after navigating the adventures of a 2,200 mile long distance relationship, 
+        a pandemic, and being relatively tired middle aged adults, we made it to the finish line! We look forward to many years together
+        spending time in nature, tending to our garden and animals, and rewatching every episode of Columbo.
         </Typography>
 
       <Grid container spacing={2} sx={{ mt: 2 }}>
